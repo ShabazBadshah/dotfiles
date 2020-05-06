5773eb76c125c38f4076b961801dd28f86ce8c88
ShabazBadshah <badshah.shabaz@gmail.com>
2020-05-06 09:46:40 -0400

    Use rename util to lowercase

--- utils/lowercase.sh
+++ utils/lowercase.sh
@@ -1,10 +1,5 @@
-# Lowercases filenames in the existing directory while replacing all the "spaces", "underscores", and "double dashes" with dashes
-
-for i in "$@";
-do
-	echo "Lowercasing filename: $i"
-	rename -f 'y/A-Z/a-z/' "$i"
-	mv -f "$i" `echo "$i" | sed s/[[:space:]]/-/g | sed s/_/-/g | sed s/--/-/g | sed s/--/-/g | tr -d '(' | tr -d ')' | tr -d ','`
-done
-
-trash icon- 
+# Lowercase all filenames
+rename -f 'y/A-Z/a-z/' *
+# Replace all spaces with underscores
+rename 's/\s/-/g' *
+trash icon-
